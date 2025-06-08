710827065c5303086f940b5300d744030fd89488
JonMS95 <jon_portugalete@hotmail.com>
2025-06-08 12:56:28 +0200

    Added necessary include statements as well as public function prototypes.

--- src/SignalHandler_api.h
+++ src/SignalHandler_api.h
@@ -1,5 +1,5 @@
-#ifndef SEVERITY_LOG_API_H
-#define SEVERITY_LOG_API_H
+#ifndef SIGNAL_HANDLER_API_H
+#define SIGNAL_HANDLER_API_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,17 +9,25 @@ extern "C" {
 /******** Include statements ********/
 /************************************/
 
+#include <stdint.h>
+
 /************************************/
 
 /***********************************/
 /******** Define statements ********/
 /***********************************/
 
+#define C_SIGNAL_HANDLER_API __attribute__((visibility("default")))
+
 /***********************************/
 
 /*************************************/
 /******** Function prototypes ********/
 /*************************************/
+
+C_SIGNAL_HANDLER_API int SignalHandlerAddCallback(void (*cb)(const int sig_num), const uint16_t sig_mask);
+C_SIGNAL_HANDLER_API int SignalHandlerGetErrorCode(void);
+C_SIGNAL_HANDLER_API const char* MutexGuardGetErrorString(const int error_code);
 
 /*************************************/
 
